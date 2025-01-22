52b3efb17e0a3edb384c2bfc6f972c08b2c64c0b
lvov <lvov.sergey.2001@gmail.com>
2025-01-22 13:35:29 +0300

    init commit

---
+++ app/Http/Controllers/SubProductController.php
@@ -0,0 +1,66 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use App\Http\Requests\StoreSubProductRequest;
+use App\Http\Requests\UpdateSubProductRequest;
+use App\Models\SubProduct;
+
+class SubProductController extends Controller
+{
+    /**
+     * Display a listing of the resource.
+     */
+    public function index()
+    {
+        //
+    }
+
+    /**
+     * Show the form for creating a new resource.
+     */
+    public function create()
+    {
+        //
+    }
+
+    /**
+     * Store a newly created resource in storage.
+     */
+    public function store(StoreSubProductRequest $request)
+    {
+        //
+    }
+
+    /**
+     * Display the specified resource.
+     */
+    public function show(SubProduct $subProduct)
+    {
+        //
+    }
+
+    /**
+     * Show the form for editing the specified resource.
+     */
+    public function edit(SubProduct $subProduct)
+    {
+        //
+    }
+
+    /**
+     * Update the specified resource in storage.
+     */
+    public function update(UpdateSubProductRequest $request, SubProduct $subProduct)
+    {
+        //
+    }
+
+    /**
+     * Remove the specified resource from storage.
+     */
+    public function destroy(SubProduct $subProduct)
+    {
+        //
+    }
+}
